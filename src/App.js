127ed72741b198a2e52e826250e634902dcce799
Ifechi Ilozor <40251709+ifechi-ilozor@users.noreply.github.com>
2020-05-08 17:34:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import "./App.css";
 import Title from "./Title";
 import FilteredList from "./FilteredList";
 
-/*var list_clues = [
-  { name: "Apple Jacks", type: "Circular" },
-  { name: "Banana Nut Cheerios", type: "Circular" },
-  { name: "Cookie Crisps", type: "Circular" },
-  { name: "Honey Nut Cheerios", type: "Circular" },
-  { name: "Fruit Loops", type: "Circular" },
-  { name: "Reeses Puffs", type: "Circular" },
-  { name: "Captain Crunch", type: "Square" },
-  { name: "Cinnamon Toast Crunch", type: "Square" },
-  { name: "Golden Grahams", type: "Square" },
-  { name: "Chex", type: "Square" },
-  { name: "Life", type: "Square" },
-  { name: "Frosted Mini Wheats", type: "Square" }
-];*/
-
 class App extends Component {
 
   constructor(props) {
